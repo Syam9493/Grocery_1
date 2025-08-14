c47ec3b8effc667220614ffd51fd573d17ea1512
Syam9493 <syamdokuparthi171@gmail.com>
2025-08-14 23:15:51 +0530

    added production  domains

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -37,7 +37,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Local backend
+        //target: "http://localhost:5000", // Local backend
+        target: "https://grocery-52wy.onrender.com", // Remote backend
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
